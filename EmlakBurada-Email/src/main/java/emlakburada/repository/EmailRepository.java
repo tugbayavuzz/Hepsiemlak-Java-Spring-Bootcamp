26aa510b4576a0e46fbed354ac0f5f886aa4d4cc
Tuğba <45331297+tugbayavuzz@users.noreply.github.com>
2022-03-13 16:32:31 +0300

    Delete EmlakBurada-Email directory

--- EmlakBurada-Email/src/main/java/emlakburada/repository/EmailRepository.java
+++
@@ -1,7 +0,0 @@
-package emlakburada.repository;
-
-import emlakburada.model.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmailRepository extends JpaRepository<Email, String> {
-}
