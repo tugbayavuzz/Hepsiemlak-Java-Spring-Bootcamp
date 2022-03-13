90d9ce2602289006437ed20f8cef81eba89538ff
Tuğba <45331297+tugbayavuzz@users.noreply.github.com>
2022-03-13 16:32:46 +0300

    Delete EmlakBurada-Banner directory

--- EmlakBurada-Banner/src/main/java/emlakburada/repository/BannerRepository.java
+++
@@ -1,9 +0,0 @@
-package emlakburada.repository;
-
-import emlakburada.model.Banner;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BannerRepository extends JpaRepository<Banner, Integer> {
-}
